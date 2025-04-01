3f7d8d4840aefdba65a5d0b0745906685b0af06c
Jb Carreon <jbcarreon212@gmail.com>
2025-04-02 02:48:23 +0800

    fix

--- static/webrings/responeko/pmoring.config.js
+++ static/webrings/responeko/pmoring.config.js
@@ -104,8 +104,8 @@ export default {
     }
 
     .phone-container {
-        height: 370px;
-        width: calc(370px * 0.5);
+        height: 374px;
+        width: calc(374px * 0.5);
 
         border: 12px solid black;
         border-radius: 10px;

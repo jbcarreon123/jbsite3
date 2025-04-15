29c89b88997ee278d3e0b6f841edd4e0b1e927d9
Jb Carreon <jbcarreon212@gmail.com>
2025-04-15 14:25:44 +0800

    vfxzgvdghdfgdfg

--- src/routes/blogs/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { json, redirect } from '@sveltejs/kit'
-import { getPosts } from '$lib/blogs/get-blogs'
-import { dev } from '$app/environment';
-import { getPostsWip } from '$lib/wip-blogs/get-blogs-wip.js';
-
-export async function load({ fetch, params }) {
-    redirect(302, '/posts')
-}
--- src/routes/blogs/[slug]/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { dev } from "$app/environment";
-import { redirect } from "@sveltejs/kit";
-
-export async function load({ params }) {
-	redirect(302, `/posts/${params.slug}`)
-}
